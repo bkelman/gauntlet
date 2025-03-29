6b5aefbcd2d13fad3eba7098084e51e39979af37
Ben Kelman <b.a.kelman@gmail.com>
2025-03-28 17:23:30 -0700

    Initial Commit

---
+++ Gauntlet Trivia/Gauntlet_TriviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gauntlet_TriviaApp.swift
+//  Gauntlet Trivia
+//
+//  Created by Ben Kelman on 3/28/25.
+//
+
+import SwiftUI
+
+@main
+struct Gauntlet_TriviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Gauntlet TriviaTests/Gauntlet_TriviaTests.swift
@@ -0,0 +1,17 @@
+//
+//  Gauntlet_TriviaTests.swift
+//  Gauntlet TriviaTests
+//
+//  Created by Ben Kelman on 3/28/25.
+//
+
+import Testing
+@testable import Gauntlet_Trivia
+
+struct Gauntlet_TriviaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
